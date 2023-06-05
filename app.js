1bdc5824003b3a7607138982ed00e032494e74d8
WebsiteAB <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-06-05 22:01:45 +0200

    Add DB connection

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-const tourRouter = require('./routes/tours')
-const userRouter = require('./routes/users')
+
+const tourRouter = require('./routes/tours');
+const userRouter = require('./routes/users');
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(express.json());
 
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
+app.use(express.static(`${__dirname}/public`));
 
-module.exports = app
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-app.listen(3000, () => {
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.PASSWORD);
+
+const dbConnect = async () => {
+  await mongoose.connect(DB);
+};
+
+dbConnect().then(() => console.log('DB connected!')).catch((err) => console.log(err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log('App is running on port 3000');
 });
